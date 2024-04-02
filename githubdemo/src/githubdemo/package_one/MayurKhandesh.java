00cfd1a307a176711228b1a3a1445f714f07ee7c
MAYUR PATIL <m.patil517152@gmail.com>
2024-04-02 22:08:40 +0530

    added code to find max num from an array

--- githubdemo/src/githubdemo/package_one/MayurKhandesh.java
+++ githubdemo/src/githubdemo/package_one/MayurKhandesh.java
@@ -1,9 +1,17 @@
 package githubdemo.package_one;
 
+import java.util.Arrays;
+
 public class MayurKhandesh {
 
 	public static void main(String[] args) {
 		// Find maximum number from array
+		
+		int[] arr = {3,4,2,6,2,4,5};
+		
+		Arrays.sort(arr);
+		
+		System.out.println("maximum number from given array is "+ arr[arr.length-1]);
 	}
 
 }
