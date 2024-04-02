8fa191b6f32eb9dd661bb358c8d3d3c832544ebd
Yogeshwari9Mane <yogimane2997@gmail.com>
2024-04-02 22:08:11 +0530

    create static method and called in main method by using class name

--- githubdemo/src/githubdemo/package_one/YogeshwariSatara.java
+++ githubdemo/src/githubdemo/package_one/YogeshwariSatara.java
@@ -4,7 +4,13 @@ public class YogeshwariSatara {
 
 	public static void main(String[] args) {
 		// create static method and call with class name
-
+		
+		YogeshwariSatara ys = new YogeshwariSatara();
+		
+		ys.staticMethod();
 	}
-
+	
+	public static void staticMethod() {
+		System.out.println("Static Method");
+	}
 }
