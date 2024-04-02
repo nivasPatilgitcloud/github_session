540dc42cf72545f12e528c0fa26cd0d445f62d0e
Jagruti patil <jagrutikpatil@98gmail.com>
2024-04-02 22:09:10 +0530

    completed given task of finding even odd numbers using stream api

--- githubdemo/src/githubdemo/package_one/JagrutiSurat.java
+++ githubdemo/src/githubdemo/package_one/JagrutiSurat.java
@@ -1,9 +1,19 @@
 package githubdemo.package_one;
 
+import java.util.Arrays;
+
 public class JagrutiSurat {
 
 	public static void main(String[] args) {
-		// Even odd using stream api
+		// Even odd using stream API
+		int[] num = { 2, 3, 4, 5, 6, 8, 7, 9, 10 };
+		// Finding even numbers
+		System.out.println("Even numbers:");
+		Arrays.stream(num).filter(n -> n % 2 == 0).forEach(System.out::println);
+
+		// Finding odd numbers
+		System.out.println("Odd numbers:");
+		Arrays.stream(num).filter(n -> n % 2 != 0).forEach(System.out::println);
 
 	}
 
