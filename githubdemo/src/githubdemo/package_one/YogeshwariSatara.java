bb65d06e80e6884ce996ce69f86c2d1028c7f15f
Yogeshwari9Mane <yogimane2997@gmail.com>
2024-04-02 23:03:49 +0530

    code updated according to static

--- githubdemo/src/githubdemo/package_one/YogeshwariSatara.java
+++ githubdemo/src/githubdemo/package_one/YogeshwariSatara.java
@@ -4,10 +4,7 @@ public class YogeshwariSatara {
 
 	public static void main(String[] args) {
 		// create static method and call with class name
-		
-		YogeshwariSatara ys = new YogeshwariSatara();
-		
-		ys.staticMethod();
+		YogeshwariSatara.staticMethod();
 	}
 	
 	public static void staticMethod() {
