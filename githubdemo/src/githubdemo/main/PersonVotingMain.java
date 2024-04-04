df90c712f1d9b321fd59ff1128f3712e5ff569fa
Yogeshwari9Mane <yogimane2997@gmail.com>
2024-04-04 20:52:12 +0530

    created object for voting app

--- githubdemo/src/githubdemo/main/PersonVotingMain.java
+++ githubdemo/src/githubdemo/main/PersonVotingMain.java
@@ -7,9 +7,10 @@ public class PersonVotingMain {
 
 	public static void main(String[] args) {
 
+//		VotingApp app = new VotingApp();
+//		app.createPersonVotingReg();
+
 		VotingApp app = new VotingApp();
 		app.createPersonVotingReg();
-
-		
 	}
 }
