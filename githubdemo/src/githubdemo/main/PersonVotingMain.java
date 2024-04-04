08bd91b76e91ee97c98a52b4da982e2f05473735
Yogeshwari9Mane <yogimane2997@gmail.com>
2024-04-04 16:33:06 +0530

    changed the modifier

--- githubdemo/src/githubdemo/main/PersonVotingMain.java
+++ githubdemo/src/githubdemo/main/PersonVotingMain.java
@@ -7,7 +7,8 @@ public class PersonVotingMain {
 
 	public static void main(String[] args) {
 
-		VotingApp.createPersonVotingReg();
+		VotingApp app = new VotingApp();
+		app.createPersonVotingReg();
 
 		
 	}
